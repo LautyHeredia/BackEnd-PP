21b5245093b611f92d05d21e1b4a175e5a90b6d0
Lautaro Heredia <103940513+LautyHeredia@users.noreply.github.com>
2023-09-18 18:23:18 -0300

    MoviesBranchUpdate

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const server = require('./config/app')
 require('./config/database')
-const port = process.env.APP_PORT;
+const port = process.env.APP_PORT || 3000;
 
 server.listen(port, () => {
     console.log(`Your application is running in the port: ${port}`)
