682c70f255100894fe26c02feb1e59b2bb06b889
Lautaro Heredia <103940513+LautyHeredia@users.noreply.github.com>
2023-09-18 20:39:27 -0300

    NewUpdate 2.0

--- Back-End/index.js
+++ Back-End/index.js
@@ -4,5 +4,5 @@ require('./config/database')
 const port = process.env.APP_PORT || 3000;
 
 server.listen(port, () => {
-    console.log(`Your application is running in the port: 3000`)
+    console.log(`Your application is running in the port: ${port}`)
 })
